e3284b9879bbeb688b82f8ff773eab24dd4395ce
poeticlama <omgsomuchhomework@gmail.com>
2026-09-06 15:47:06 -0400

    feat: simple timezones options

---
+++ src/index.ts
@@ -0,0 +1,26 @@
+export type TimezoneType = {
+	tzCode: string
+	label: string | null | undefined
+}
+
+export function getIntlTimezones() {
+	return Intl.supportedValuesOf("timeZone")
+}
+
+export function createTimezonezLabelMap<Timezone extends TimezoneType>(
+	timezones: Timezone[],
+) {
+	return new Map(timezones.map((timezone) => [timezone.tzCode, timezone.label]))
+}
+
+export function createTimezonesOptions<Timezone extends TimezoneType>(
+	timezones: Timezone[],
+) {
+	const labelMap = createTimezonezLabelMap(timezones)
+	const intlTimezones = getIntlTimezones()
+
+	intlTimezones.map((timezone) => ({
+		label: labelMap.get(timezone) ?? timezone,
+		value: timezone,
+	}))
+}
